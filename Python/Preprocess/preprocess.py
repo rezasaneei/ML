5828330fe93f6a51bcac11ba9003cddc41bb74da
rezasaneei <49787609+rezasaneei@users.noreply.github.com>
2019-05-01 02:04:10 +0200

    added preprocess

---
+++ Python/Preprocess/preprocess.py
@@ -0,0 +1,512 @@
+﻿
+# coding: utf-8
+
+# # Summarizing Text with Amazon Reviews
+
+# The objective of this project is to build a model that can create relevant summaries for reviews written about fine foods sold on Amazon. This dataset contains above 500,000 reviews, and is hosted on [Kaggle.](https://www.kaggle.com/snap/amazon-fine-food-reviews)
+# 
+# To build our model we will use a two-layered bidirectional RNN with LSTMs on the input data and two layers, each with an LSTM using bahdanau attention on the target data. [Jaemin Cho's tutorial](https://github.com/j-min/tf_tutorial_plus/tree/master/RNN_seq2seq/contrib_seq2seq) for seq2seq was really helpful to get the code in working order because this is my first project with TensorFlow 1.1; some of the functions are very different from 1.0. The architecture for this model is similar to Xin Pan's and Peter Liu's, here's their [GitHub page.](https://github.com/tensorflow/models/tree/master/textsum)
+# 
+# The sections of this project are:
+# - Inspecting the Data
+# - Preparing the Data
+# - Building the Model
+# - Training the Model
+# - Making Our Own Summaries
+
+
+import pandas as pd
+import numpy as np
+#import tensorflow as tf
+import re
+from nltk.corpus import stopwords
+import pickle
+#import time
+#from tensorflow.python.layers.core import Dense
+#from tensorflow.python.ops.rnn_cell_impl import _zero_state_tensors
+#print('TensorFlow Version: {}'.format(tf.__version__))
+
+
+
+reviews = pd.read_csv("Reviews.csv")
+
+
+
+reviews.shape
+
+
+
+reviews.head()
+
+
+# Check for any nulls values
+reviews.isnull().sum()
+
+
+
+# Remove null values and unneeded features
+reviews = reviews.dropna()
+reviews = reviews.drop(['Id','ProductId','UserId','ProfileName','HelpfulnessNumerator','HelpfulnessDenominator',
+                        'Score','Time'], 1)
+reviews = reviews.reset_index(drop=True)
+
+
+
+reviews.head()
+
+
+
+# Inspecting some of the reviews
+for i in range(5):
+    print("Review #",i+1)
+    print(reviews.Summary[i])
+    print(reviews.Text[i])
+    print()
+
+
+# ## Preparing the Data
+
+
+# A list of contractions from http://stackoverflow.com/questions/19790188/expanding-english-language-contractions-in-python
+contractions = { 
+"ain't": "am not",
+"aren't": "are not",
+"can't": "cannot",
+"can't've": "cannot have",
+"'cause": "because",
+"could've": "could have",
+"couldn't": "could not",
+"couldn't've": "could not have",
+"didn't": "did not",
+"doesn't": "does not",
+"don't": "do not",
+"hadn't": "had not",
+"hadn't've": "had not have",
+"hasn't": "has not",
+"haven't": "have not",
+"he'd": "he would",
+"he'd've": "he would have",
+"he'll": "he will",
+"he's": "he is",
+"how'd": "how did",
+"how'll": "how will",
+"how's": "how is",
+"i'd": "i would",
+"i'll": "i will",
+"i'm": "i am",
+"i've": "i have",
+"isn't": "is not",
+"it'd": "it would",
+"it'll": "it will",
+"it's": "it is",
+"let's": "let us",
+"ma'am": "madam",
+"mayn't": "may not",
+"might've": "might have",
+"mightn't": "might not",
+"must've": "must have",
+"mustn't": "must not",
+"needn't": "need not",
+"oughtn't": "ought not",
+"shan't": "shall not",
+"sha'n't": "shall not",
+"she'd": "she would",
+"she'll": "she will",
+"she's": "she is",
+"should've": "should have",
+"shouldn't": "should not",
+"that'd": "that would",
+"that's": "that is",
+"there'd": "there had",
+"there's": "there is",
+"they'd": "they would",
+"they'll": "they will",
+"they're": "they are",
+"they've": "they have",
+"wasn't": "was not",
+"we'd": "we would",
+"we'll": "we will",
+"we're": "we are",
+"we've": "we have",
+"weren't": "were not",
+"what'll": "what will",
+"what're": "what are",
+"what's": "what is",
+"what've": "what have",
+"where'd": "where did",
+"where's": "where is",
+"who'll": "who will",
+"who's": "who is",
+"won't": "will not",
+"wouldn't": "would not",
+"you'd": "you would",
+"you'll": "you will",
+"you're": "you are"
+}
+
+def clean_text(text, remove_stopwords = True):
+    '''Remove unwanted characters, stopwords, and format the text to create fewer nulls word embeddings'''
+    
+    # Convert words to lower case
+    text = text.lower()
+    
+    # Replace contractions with their longer forms 
+    if True:
+        text = text.split()
+        new_text = []
+        for word in text:
+            if word in contractions:
+                new_text.append(contractions[word])
+            else:
+                new_text.append(word)
+        text = " ".join(new_text)
+    
+    # Format words and remove unwanted characters
+    text = re.sub(r'<br >', ' ', text) 
+    text = re.sub(r'<br />', ' ', text)
+    text = re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+    text = re.sub(r'\<a href', ' ', text)
+    text = re.sub(r'&amp;', '', text) 
+    text = re.sub(r'[_"\-;%()|+&=*%.,!?:#$@\[\]/]', ' ', text)
+       
+
+    text = re.sub(r'\'', ' ', text)
+    
+    # Optionally, remove stop words
+    if remove_stopwords:
+        text = text.split()
+        stops = set(stopwords.words("english"))
+        text = [w for w in text if not w in stops]
+        text = " ".join(text)
+
+    return text
+
+
+# We will remove the stopwords from the texts because they do not provide much use for training our model. However, we will keep them for our summaries so that they sound more like natural phrases. 
+
+
+# Clean the summaries and texts
+clean_summaries = []
+for summary in reviews.Summary:
+    clean_summaries.append(clean_text(summary, remove_stopwords=False))
+print("Summaries are complete.")
+
+clean_texts = []
+for text in reviews.Text:
+    clean_texts.append(clean_text(text))
+print("Texts are complete.")
+
+
+# Inspect the cleaned summaries and texts to ensure they have been cleaned well
+for i in range(5):
+    print("Clean Review #",i+1)
+    print(clean_summaries[i])
+    print(clean_texts[i])
+    print()
+
+
+
+def count_words(count_dict, text):
+    '''Count the number of occurrences of each word in a set of text'''
+    for sentence in text:
+        for word in sentence.split():
+            if word not in count_dict:
+                count_dict[word] = 1
+            else:
+                count_dict[word] += 1
+
+
+word_counts = {}
+
+count_words(word_counts, clean_summaries)
+count_words(word_counts, clean_texts)
+            
+print("Size of Vocabulary:", len(word_counts))
+
+
+
+
+embeddings_index = {}
+with open('numberbatch-en-17.02.txt', encoding='utf-8') as f:
+    for line in f:
+        values = line.split(' ')
+        word = values[0]
+        embedding = np.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = embedding
+
+print('Word embeddings:', len(embeddings_index))
+
+
+missing_words = 0
+threshold = 20
+
+for word, count in word_counts.items():
+    if count > threshold:
+        if word not in embeddings_index:
+            missing_words += 1
+            
+missing_ratio = round(missing_words/len(word_counts),4)*100
+            
+print("Number of words missing from CN:", missing_words)
+print("Percent of words that are missing from vocabulary: {}%".format(missing_ratio))
+
+
+#dictionary to convert words to integers
+vocab_to_int = {} 
+
+value = 0
+for word, count in word_counts.items():
+    if count >= threshold or word in embeddings_index:
+        vocab_to_int[word] = value
+        value += 1
+
+# Special tokens that will be added to our vocab
+codes = ["<UNK>","<PAD>","<EOS>","<GO>"]   
+
+# Add codes to vocab
+for code in codes:
+    vocab_to_int[code] = len(vocab_to_int)
+
+# Dictionary to convert integers to words
+int_to_vocab = {}
+for word, value in vocab_to_int.items():
+    int_to_vocab[value] = word
+
+#save int to vocab dictionary in a file
+
+a = {'hello': 'world'}
+
+with open('intToVocabDict', 'wb') as handle:
+    pickle.dump(int_to_vocab, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+usage_ratio = round(len(vocab_to_int) / len(word_counts),4)*100
+
+print("Total number of unique words:", len(word_counts))
+print("Number of words we will use:", len(vocab_to_int))
+print("Percent of words we will use: {}%".format(usage_ratio))
+
+
+
+# Need to use 300 for embedding dimensions to match CN's vectors.
+embedding_dim = 300
+nb_words = len(vocab_to_int)
+
+# Create matrix with default values of zero
+word_embedding_matrix = np.zeros((nb_words, embedding_dim), dtype=np.float32)
+for word, i in vocab_to_int.items():
+    if word in embeddings_index:
+        word_embedding_matrix[i] = embeddings_index[word]
+    else:
+        # If word not in CN, create a random embedding for it
+        new_embedding = np.array(np.random.uniform(-1.0, 1.0, embedding_dim))
+        embeddings_index[word] = new_embedding
+        word_embedding_matrix[i] = new_embedding
+
+# Check if value matches len(vocab_to_int)
+print(len(word_embedding_matrix))
+
+
+
+def convert_to_ints(text, word_count, unk_count, eos=False):
+    '''Convert words in text to an integer.
+       If word is not in vocab_to_int, use UNK's integer.
+       Total the number of words and UNKs.
+       Add EOS token to the end of texts'''
+    ints = []
+    for sentence in text:
+        sentence_ints = []
+        for word in sentence.split():
+            word_count += 1
+            if word in vocab_to_int:
+                sentence_ints.append(vocab_to_int[word])
+            else:
+                sentence_ints.append(vocab_to_int["<UNK>"])
+                unk_count += 1
+        if eos:
+            sentence_ints.append(vocab_to_int["<EOS>"])
+        ints.append(sentence_ints)
+    return ints, word_count, unk_count
+
+
+
+# Apply convert_to_ints to clean_summaries and clean_texts
+word_count = 0
+unk_count = 0
+
+int_summaries, word_count, unk_count = convert_to_ints(clean_summaries, word_count, unk_count)
+int_texts, word_count, unk_count = convert_to_ints(clean_texts, word_count, unk_count, eos=True)
+
+unk_percent = round(unk_count/word_count,4)*100
+
+print("Total number of words in headlines:", word_count)
+print("Total number of UNKs in headlines:", unk_count)
+print("Percent of words that are UNK: {}%".format(unk_percent))
+
+
+def create_lengths(text):
+    '''Create a data frame of the sentence lengths from a text'''
+    lengths = []
+    for sentence in text:
+        lengths.append(len(sentence))
+    return pd.DataFrame(lengths, columns=['counts'])
+
+
+lengths_summaries = create_lengths(int_summaries)
+lengths_texts = create_lengths(int_texts)
+
+print("Summaries:")
+print(lengths_summaries.describe())
+print()
+print("Texts:")
+print(lengths_texts.describe())
+
+
+
+# Inspect the length of texts
+print(np.percentile(lengths_texts.counts, 90))
+print(np.percentile(lengths_texts.counts, 95))
+print(np.percentile(lengths_texts.counts, 99))
+
+
+
+# Inspect the length of summaries
+print(np.percentile(lengths_summaries.counts, 90))
+print(np.percentile(lengths_summaries.counts, 95))
+print(np.percentile(lengths_summaries.counts, 99))
+
+
+
+def unk_counter(sentence):
+    '''Counts the number of time UNK appears in a sentence.'''
+    unk_count = 0
+    for word in sentence:
+        if word == vocab_to_int["<UNK>"]:
+            unk_count += 1
+    return unk_count
+
+
+sorted_summaries = []
+sorted_texts = []
+# Original data
+sorted_original_summaries=[]
+sorted_original_texts = []
+
+max_text_length = 84
+max_summary_length = 13
+min_length = 2
+unk_text_limit = 1
+unk_summary_limit = 0
+
+for length in range(min(lengths_texts.counts), max_text_length): 
+    for count, words in enumerate(int_summaries):
+        if (len(int_summaries[count]) >= min_length and
+            len(int_summaries[count]) <= max_summary_length and
+            len(int_texts[count]) >= min_length and
+            unk_counter(int_summaries[count]) <= unk_summary_limit and
+            unk_counter(int_texts[count]) <= unk_text_limit and
+            length == len(int_texts[count])
+           ):
+            sorted_summaries.append(int_summaries[count])
+            sorted_texts.append(int_texts[count])
+            
+            sorted_original_summaries.append(reviews.Summary[count])
+            sorted_original_texts.append(reviews.Text[count])
+        
+# Compare lengths to ensure they match
+print(len(sorted_summaries))
+print(len(sorted_texts))
+
+
+def pad_sentence_batch(sentence_batch):
+    """Pad sentences with <PAD> so that each sentence of a batch has the same length"""
+    max_sentence = max([len(sentence) for sentence in sentence_batch])
+    return [sentence + [vocab_to_int['<PAD>']] * (max_sentence - len(sentence)) for sentence in sentence_batch]
+
+
+
+def get_batches(summaries, texts, batch_size):
+    """Batch summaries, texts, and the lengths of their sentences together"""
+    for batch_i in range(0, len(texts)//batch_size):
+        start_i = batch_i * batch_size
+        summaries_batch = summaries[start_i:start_i + batch_size]
+        texts_batch = texts[start_i:start_i + batch_size]
+        pad_summaries_batch = np.array(pad_sentence_batch(summaries_batch))
+        pad_texts_batch = np.array(pad_sentence_batch(texts_batch))
+        
+        # Need the lengths for the _lengths parameters
+        pad_summaries_lengths = []
+        for summary in pad_summaries_batch:
+            pad_summaries_lengths.append(len(summary))
+        
+        pad_texts_lengths = []
+        for text in pad_texts_batch:
+            pad_texts_lengths.append(len(text))
+        
+        yield pad_summaries_batch, pad_texts_batch, pad_summaries_lengths, pad_texts_lengths
+
+
+epochs = 100
+batch_size = 64
+rnn_size = 256
+num_layers = 2
+learning_rate = 0.005
+keep_probability = 0.75
+
+start = 1
+end = start + 500000
+sorted_summaries_short = sorted_summaries[start:end]
+sorted_texts_short = sorted_texts[start:end]
+
+# Original data 
+sorted_original_summaries_short = sorted_original_summaries[start:end]
+sorted_original_texts_short = sorted_original_texts[start:end]
+
+print("The shortest text length:", len(sorted_texts_short[0]))
+print("The longest text length:",len(sorted_texts_short[-1]))
+
+print("Length of the summaries list:",len(sorted_summaries))
+print("Legth of the shorted summaries:",len(sorted_summaries_short))
+
+with open('Summaries-file','wb') as fb:
+    pickle.dump(sorted_summaries_short,fb)
+    
+with open('Texts-file','wb') as fb:
+    pickle.dump(sorted_texts_short,fb)   
+    
+    
+#Create file of original data lists
+with open('Original-Summaries-file','wb') as fb:
+    pickle.dump(sorted_original_summaries_short,fb)
+    
+with open('Original-Texts-file','wb') as fb:
+    pickle.dump(sorted_original_texts_short,fb) 
+
+def text_to_seq(text):
+    '''Prepare the text for the model'''
+    
+    text = clean_text(text)
+    return [vocab_to_int.get(word, vocab_to_int['<UNK>']) for word in text.split()]
+
+
+random = np.random.randint(0,len(clean_texts))
+input_sentence = clean_texts[random]
+text = text_to_seq(clean_texts[random])
+
+checkpoint = "./best_model.ckpt"
+
+
+
+# Remove the padding from the tweet
+pad = vocab_to_int["<PAD>"] 
+
+print('Original Text:', input_sentence)
+
+print('\nText')
+print('  Word Ids:    {}'.format([i for i in text]))
+print('  Input Words: {}'.format(" ".join([int_to_vocab[i] for i in text])))
+
+print('\nSummary')
+
+
+
